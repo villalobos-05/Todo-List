9a53a005fc0d309344586c1f42bf31bcec82534e
Daniel <danielvillalobosfdz@gmail.com>
2024-11-08 11:57:40 +0100

    fix parameters

--- src/components/icons/AddIcon.jsx
+++ src/components/icons/AddIcon.jsx
@@ -1,5 +1,5 @@
 export function AddIcon () {
   return (
-    <svg className='size-[32px]' width='64px' height='64px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' stroke='#cbd5e1'><g id='SVGRepo_bgCarrier' stroke-width='0' /><g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round' /><g id='SVGRepo_iconCarrier'> <g id='Edit / Add_Plus'> <path id='Vector' d='M6 12H12M12 12H18M12 12V18M12 12V6' stroke='#cbd5e1' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' /> </g> </g></svg>
+    <svg className='size-[32px]' width='64px' height='64px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' stroke='#cbd5e1'><g id='SVGRepo_bgCarrier' strokeWidth='0' /><g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round' /><g id='SVGRepo_iconCarrier'> <g id='Edit / Add_Plus'> <path id='Vector' d='M6 12H12M12 12H18M12 12V18M12 12V6' stroke='#cbd5e1' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' /> </g> </g></svg>
   )
 }
